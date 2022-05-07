245c6c3419fa9ca75cb470bf05bf71721c45c5a2
dododuoduo <zenial069@gmail.com>
2022-05-07 14:50:41 +1000

    Initial Commit

---
+++ AssignmentTest2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AssignmentTest2
+//
+//  Created by Yihan LIU on 7/5/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
