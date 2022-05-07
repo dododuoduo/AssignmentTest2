f38891976a646618026972c399cf1d75dd08639c
dododuoduo <zenial069@gmail.com>
2022-05-07 15:00:21 +1000

    Update two files
    Add MainPageVC
    Remove comment from ViewController

---
+++ AssignmentTest2/Controllers/MainPageVC.swift
@@ -0,0 +1,29 @@
+//
+//  MainPageVC.swift
+//  AssignmentTest2
+//
+//  Created by Yihan LIU on 7/5/2022.
+//
+
+import UIKit
+
+class MainPageVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
